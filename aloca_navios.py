10a0e346b435faa37c48a78dfbdb89cd908e65bb
Danilo Ramos <danilocr@al.insper.edu.br>
2024-04-26 21:52:18 -0300

    Funcao de teste de suporte sem import

--- aloca_navios.py
+++ aloca_navios.py
@@ -1,5 +1,27 @@
 import random
-import suporta_posicao as sup
+#import suporta_posicao as sup
+def posicao_suporta(mapa, blocos, linha, coluna, orientacao):
+    if orientacao.strip().lower() == 'v':
+        final = linha + blocos
+        i = linha
+        if final > len(mapa):
+            return False
+        else:
+            while i < final:
+                if mapa[i][coluna].strip() != '':
+                    return False
+                i += 1
+    elif orientacao.strip().lower() == 'h':
+        final = coluna + blocos 
+        i = coluna
+        if final > len(mapa[0]):
+            return False
+        else:
+            while i < final:
+                if mapa[linha][i].strip() != '':
+                    return False
+                i += 1
+    return True
 
 def aloca_navios(mapa, navios):  
     for blocos in navios:
@@ -7,7 +29,7 @@ def aloca_navios(mapa, navios):
         coluna = random.randint(0, len(mapa) - 1)
         orientacao = random.choice(['h', 'v'])
 
-        while not sup.posicao_suporta(mapa, blocos, linha, coluna, orientacao):
+        while not posicao_suporta(mapa, blocos, linha, coluna, orientacao):
             linha = random.randint(0, len(mapa) - 1)
             coluna = random.randint(0, len(mapa) -1)
             orientacao = random.choice(['h', 'v'])
