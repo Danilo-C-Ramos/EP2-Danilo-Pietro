54413bfb374e35425cde0796d0e3f670f4aa63ee
Danilo <danilocr@al.insper.edu.br>
2024-04-29 07:50:41 -0300

    Função cria mapa

--- cria_mapa.py
+++ cria_mapa.py
@@ -1,4 +1,3 @@
-"""
 def cria_mapa(N):
     lisf=[]
     lis=[]
@@ -7,9 +6,3 @@ def cria_mapa(N):
         for j in range (len(lis)):
             lisf.append(lis)
     return lisf
-"""
-def cria_mapa(N):
-    mapa = [
-        [' ' for coluna in range(N)] #Cria uma linha com N " ", ou seja, N colunas
-             for linha in range(N)] # Cria N linhas
-    return mapa
